bdb8455c5bb73db26ef3d5822f3a9a46d60c0d7c
ariram <vaasit@gmail.com>
2023-04-02 15:46:02 +0000

    add

--- providers.tf
+++ providers.tf
@@ -2,14 +2,15 @@ provider "aws"{
 
    # alias = "primary"
    region = "us-west-2"
+   access_key = "AKIATKVJB75CCQPWBJLH"
+   secret_key = "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"
    
-   assume_role{
-     role_arn = format("arn:aws:iam::%s:role/svc.terrafor.role",var.aws_account_id)
-    }
+   # assume_role{
+   #   role_arn = format("arn:aws:iam::%s:role/svc.terrafor.role",var.aws_account_id)
+   #  }
 
    # assume_role{
    #   role_arn = "arn:aws:iam::229061558084:role/ari-clouddemo"
    #  }
-
 }
 
