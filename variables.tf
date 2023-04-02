3b7b8940bdd931cd99cad55feb6a9a307daeef14
ariram <vaasit@gmail.com>
2023-04-02 17:17:24 +0000

    add

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "aws_account_id"{
-	type = string
-}
+# variable "aws_account_id"{
+# 	type = string
+# }
 
-variable "AWS_REGION"{
-	type = string
-}
+# variable "AWS_REGION"{
+# 	type = string
+# }
 
